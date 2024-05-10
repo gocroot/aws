fd7d9b1ac5caefd65dfc990bc07844517a8bc217
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-11 06:47:15 +0700

    secret

--- controller/whatsauth.go
+++ controller/whatsauth.go
@@ -15,11 +15,14 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	for key, value := range request.Headers {
 		fmt.Printf("    %s: %s\n", key, value)
 	}
-	key := "Secret"
-	if value, exists := request.Headers[key]; exists {
-		fmt.Printf("Key '%s' found with value '%s'\n", key, value)
-	} else {
-		fmt.Printf("Key '%s' not found\n", key)
+	fmt.Println("apakah ada secret?")
+	fmt.Println(request.Headers["secret"])
+	key1 := "secret"
+	key2 := "Secret"
+	if value, exists := request.Headers[key1]; exists {
+		fmt.Printf("Key '%s' found with value '%s'\n", key1, value)
+	} else if value, exists := request.Headers[key2]; exists {
+		fmt.Printf("Key '%s' found with value '%s'\n", key1, value)
 	}
 
 	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
