25375dae3a6e3ce3d00fb513e7cc21a0d923dbc8
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-10 15:36:07 +0700

    coba

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"gocroot/model"
+	"strings"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -16,6 +19,38 @@ func HandleRequest(ctx context.Context, event *model.IteungMessage) (*string, er
 	return &message, nil
 }
 
+func handler(request events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+	token := request.AuthorizationToken
+	tokenSlice := strings.Split(token, " ")
+	var bearerToken string
+	if len(tokenSlice) > 1 {
+		bearerToken = tokenSlice[len(tokenSlice)-1]
+	}
+	if bearerToken != "hello" {
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized")
+	}
+
+	return generatePolicy("user", "Allow", request.MethodArn), nil
+}
+
+func generatePolicy(principalID, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalID}
+
+	if effect != "" && resource != "" {
+		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   effect,
+					Resource: []string{resource},
+				},
+			},
+		}
+	}
+	return authResponse
+}
+
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(handler)
 }
